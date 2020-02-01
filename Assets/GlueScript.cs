2e47a0d3d1f070c15f40690c2a155d4371193331
SeamusMcFarland <60547736+SeamusMcFarland@users.noreply.github.com>
2020-02-01 09:57:29 -0600

    Added Glue, Player, and the scripts for each. Also made camera child to player object

---
+++ Assets/GlueScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlueScript : MonoBehaviour
+{
+    float size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = 0.2f; // same as minimum constant on PlayerScript
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetSize()
+    {
+        transform.localScale = new Vector3(size, size *, size *);
+    }
+
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+
+    public GameObject glue;
+    GlueScript glueS;
+
+    bool active;
+    float cooldown;
+    const float BASE_COOLDOWN = 0.5f;
+    bool rightClicking; // continually remains true until click is released
+
+    float glueBuildup;
+    const float GLUE_RATE_INCREASE = 0.01f;
+    const float MINIMUM_GLUE_BUILDUP = 0.2f;
+    const float MAXIMUM_GLUE_BUILDUP = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = true;
+        glueS = glue.GetComponent<GlueScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightClicking == true)
+        {
+            BuildupGlue();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            rightClicking = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            ShootGlue();
+            rightClicking = false;
+        }
+    }
+
+    private void ShootGlue()
+    {
+        //NEED METHOD CALL ON GLUE OBJECT SCRIPT SHOOTING GLUE
+        PropelBackward();
+    }
+
+    private void PropelBackward()
+    {
+
+    }
+
+    private void BuildupGlue()
+    {
+        glueBuildup += GLUE_RATE_INCREASE;
+        glueS.SetSize();
+    }
+
+}
